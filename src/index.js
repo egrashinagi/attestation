888b2d020dd603f0948b128304593034227397e4
Egrashina Galina <egrashina.gi@gmail.com>
2017-05-18 14:58:23 +0300

    подключены темы

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-angular.module('MyApp', ['ngMaterial']);
+angular
+    .module('MyApp', ['ngMaterial'])
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue-grey')
+            .accentPalette('blue-grey')
+            .warnPalette('deep-orange')
+            .backgroundPalette('blue-grey')
+    })
+;
 
