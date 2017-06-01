f879f6e03a24418c605dd039bf09386781c20111
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2017-06-01 15:09:26 +0300

    remove redundant controller

--- src/index.js
+++ src/index.js
@@ -8,14 +8,15 @@ angular
             .backgroundPalette('blue-grey')
     })
 
-    .controller('myChips', ['$scope', ($scope) => {
-        $scope.myItems = ['Москва', 'Санкт-Петербург', 'Балашиха', 'Самара', 'Тула'];
-    }])
-
     .controller('indexController', ['$scope', '$window', ($scope, $window) => {
+
+        $scope.myItems = ['Москва', 'Санкт-Петербург', 'Балашиха', 'Самара', 'Тула'];
+
         $scope.open = (name) => {
             $window.open(name, '_blank');
         };
+        $scope.close = (name) => {
+            $window.close();
+        };
     }])
-
 ;
